040a7114ca78ebab8e3fa3bf9ca79ebcfa9b14bf
janaka@codecommit-at-789734661575 <janaka@blackswan-technologies.com>
2023-08-11 15:50:53 +0530

    Add project files.

---
+++ CrudApp/Controllers/TasksController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            var tasks = new string[] { "Task 1", "Task 2", "Task 3" };
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public IActionResult NewTaks()
+        {
+            return Ok();
+        }
+
+    }
+}
